6a85600007e99af08a2e2d30b3765877338254f3
bkchu-clients <bkchu.clients@gmail.com>
2019-05-02 21:09:53 -0500

    remove space

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
       '-webkit-transform': 'rotate(' + degrees + 'deg)',
       '-moz-transform': 'rotate(' + degrees + 'deg)',
       '-ms-transform': 'rotate(' + degrees + 'deg)',
-      transform: 'rotate(' + degrees + 'deg)' 
+      transform: 'rotate(' + degrees + 'deg)'
     });
     return $(this);
   };
